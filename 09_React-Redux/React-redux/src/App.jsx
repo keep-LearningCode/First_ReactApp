447a09b358ffd8f139fa94b3cae672e568e07c5d
keep-LearningCode <keepLearningWith.divya19@gmail.com>
2024-08-21 21:34:32 +0530

    redux

---
+++ 09_React-Redux/React-redux/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>todo using react-redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
